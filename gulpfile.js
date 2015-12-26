9c81a7feab2013e37060e531147f5b426f7980e8
plsr <christianpoplawski@gmail.com>
2015-12-26 19:07:49 +0100

    Implemented gulp for development
    All SCSS source files are now located in /style.
    The output is one single style.css file as required by wordpress.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./style/**/*.scss')
+      .pipe(sass({
+        includePaths: ['./style'],
+        outputStyle: 'expanded'
+      }))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./style/**/*.scss', ['sass']);
+});
